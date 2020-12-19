4cd524a290ce5be4bd87d0c03c0137d0b1b66840
andrewsilent <36265247+andrewsilent@users.noreply.github.com>
2020-12-19 11:51:47 +0200

    fix image,title,paragraph,css classes,short script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,7 @@
 let overlay = document.getElementById('overlay');
 let button = document.getElementById('button');
-button.addEventListener('click',overlayShow);
-function overlayShow() {
-    
-    if (overlay.classList.contains('show')) {
-        overlay.classList.remove('show');
-        button.innerHTML='Show grid';
-    }
-    else {
-        overlay.classList.add('show');
-        button.innerHTML='Remove grid';
-    }
-}
+button.addEventListener('click', function() { 
+    overlay.classList.contains('show')?
+    (overlay.classList.remove('show'), button.innerHTML='Show grid'):
+    (overlay.classList.add('show'), button.innerHTML='Remove grid')
+})
